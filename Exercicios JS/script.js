22adbb8bfd7346e424240b37cd7b456f4e8659ea
Guilherme Cruz <136821509+guigolinho@users.noreply.github.com>
2025-05-22 20:47:45 -0300

    Update script.js

--- Exercicios JS/script.js
+++ Exercicios JS/script.js
@@ -1,24 +1,25 @@
 // EXERCÍCIO 1 --------------------------------------------------------
 const msPorDia = 1000 * 60 * 60 * 24;
 const hoje = new Date();
-const dataFerias = new Date("2025-07-01")
-const dataNiver = new Date("2026-03-28")
+const dataFerias = new Date("2025-07-01");
+const dataNiver = new Date("2026-03-28");
 
 function contarDias(data) {
-	return Math.ceil((data - hoje) / msPorDia);
+  return Math.ceil((data - hoje) / msPorDia);
 }
 
 document.getElementById("saidaFerias").textContent =
-	`Faltam ${contarDias(dataFerias)} dia(s) para as férias (01/07/2025).`;
+  `Faltam ${contarDias(dataFerias)} dia(s) para as férias (01/07/2025).`;
 
 document.getElementById("saidaNiver").textContent =
-	`Faltam ${contarDias(dataNiver)} dia(s) para meu próximo o aniversário (28/03/2026).`;
+  `Faltam ${contarDias(dataNiver)} dia(s) para meu próximo o aniversário (28/03/2026).`;
 
 // EXERCÍCIO 2 --------------------------------------------------------
 
 function calcularIMC() {
-	let peso = parseFloat(document.getElementById("peso").value);
-	let altura = parseFloat(document.getElementById("altura").value);
+  let peso = parseFloat(document.getElementById("peso").value);
+  let altura = parseFloat(document.getElementById("altura").value);
+
   if (isNaN(peso) || isNaN(altura) || altura <= 0 || peso <= 0) {
     return -1;
   }
@@ -43,10 +44,10 @@ function formatarIMC(imc) {
 }
 
 document.getElementById("botaoIMC").addEventListener("click", () => {
-  imcCalculado = calcularIMC();
-	let resultado = document.getElementById("resultadoIMC");
-	
-	if (imcCalculado === -1) {
+  const imcCalculado = calcularIMC();
+  const resultado = document.getElementById("resultadoIMC");
+
+  if (imcCalculado === -1) {
     resultado.textContent = "Dados inválidos.";
   } else {
     resultado.textContent = formatarIMC(imcCalculado);
